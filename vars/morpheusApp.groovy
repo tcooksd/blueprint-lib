71f6490cf88265129df4eeb5a6655e73ef2bc50d
tcooksd@gmail.com <tcooksd@gmail.com>
2018-09-25 13:17:21 -0700

    adding an application template for provisioning apps .

--- vars/morpheusApp.groovy
+++ vars/morpheusApp.groovy
@@ -1,6 +1,6 @@
-import org.morpeusclient.JenkinsHttpClient
+import org.blueprint01.application01
 
-def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
+/* def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
 	JenkinsHttpClient http = new JenkinsHttpClient()
 	http.postJson(morpheusUrl, postBody, bearerToken)
 }
@@ -9,3 +9,18 @@ def pullJson(String morpheusUrl,String bearerToken) {
 	JenkinsHttpClient http = new JenkinsHttpClient()
 	http.get(morpheusUrl, bearerToken)
 }
+*/
+
+/* def buildBlueprintJson(Map<?, ?>) {} */
+
+def buildAppJson(Map<?, ?> postbody01) {
+	AppJson jsonret01 = new AppJson()
+	jsonret01.postBody01(postbody01)
+
+}
+
+
+/* def buildTaskJson(Map<?, ?>) {}
+
+def buildWorkflowJson(Map<?, ?>) {}
+*/
