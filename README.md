304dd369ac88388872ecd43f85ebfb1ba83d11c2
tcooksd@gmail.com <tcooksd@gmail.com>
2018-09-25 11:05:49 -0700

    adding new lib modules

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# jenkins-morph-lib-
 # blueprint-lib
---
+++ vars/morpheusApp.groovy
@@ -0,0 +1,11 @@
+import org.morpeusclient.JenkinsHttpClient
+
+def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
+	JenkinsHttpClient http = new JenkinsHttpClient()
+	http.postJson(morpheusUrl, postBody, bearerToken)
+}
+
+def pullJson(String morpheusUrl,String bearerToken) {
+	JenkinsHttpClient http = new JenkinsHttpClient()
+	http.get(morpheusUrl, bearerToken)
+}
