1b8c93b383d43fb17acaedc0795241698c71c28f
tcooksd@gmail.com <tcooksd@gmail.com>
2018-09-25 17:08:45 -0700

    vzxcv

--- vars/MorpheusJson.groovy
+++ vars/MorpheusJson.groovy
@@ -13,7 +13,7 @@ def pullJson(String morpheusUrl,String bearerToken) {
 
 /* def buildBlueprintJson(Map<?, ?>) {} */
 
-def buildAppJson(String postbody01) {
+def buildAppJson(Map<?, ?> postbody01) {
 	prinln postbody01
 
 
